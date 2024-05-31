4b014da329c2adab2f82d4ce03c9e0eb4d52a227
Masya <zhongli1210@gmail.com>
2024-05-31 19:43:11 +0300

    first commit

---
+++ sources/manya.cpp
@@ -0,0 +1,12 @@
+#include "../headers/libs.hpp"
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
